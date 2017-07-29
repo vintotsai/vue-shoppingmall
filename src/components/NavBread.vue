3ba05764ea1f1e4f1a7632e99318ed766d12d2b1
Mac <Tsai@MacdeMacBook-Pro-2.local>
2017-07-29 20:41:12 +0800

    🎂组件化实现

---
+++ src/components/NavBread.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="nav-breadcrumb-wrap">
+    <div class="container">
+      <nav class="nav-breadcrumb">
+        <a href="/">Home</a>
+        <!-- <span>Goods</span> -->
+        <!-- 插槽，以供不同页面使用 -->
+        <slot></slot> 
+      </nav>
+    </div>
+  </div>
+</template>
