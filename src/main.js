ea6b5f216ccce661e88b8256dc94f7e8b3d0b66e
vincentotsai <totsai@139.com>
2017-08-08 12:12:35 +0800

    [update]vuex功能拆分

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Vuex from 'vuex'
+// import Vuex from 'vuex'
+import {store} from './store'
 
 import './assets/css/base.css'
 import './assets/css/checkout.css'
@@ -32,22 +33,7 @@ Vue.use(infiniteScroll)
 // elm ui
 Vue.use(ElementUI)
 
-Vue.use(Vuex)
-
-const store = new Vuex.Store({
-  state:{
-    nickName:'',
-    cartCount:0 
-  },
-  mutations:{
-    updateUserInfo(state, nickName){
-      state.nickName = nickName
-    },
-    updateCartCount(state, cartCount){
-      state.cartCount += cartCount
-    }
-  }
-})
+// Vue.use(Vuex)
 
 // 定义全局过滤器
 Vue.filter("currency",currency)
---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+  state:{
+    nickName:'',
+    cartCount:0 
+  },
+  mutations:{
+    updateUserInfo(state, nickName){
+      state.nickName = nickName
+    },
+    updateCartCount(state, cartCount){
+      state.cartCount += cartCount
+    }
+  }
+})
